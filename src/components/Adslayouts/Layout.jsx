3e5d1c6aa4565c2342d5d12b18d6392ad24d7fce
NikitaBenzin <83305705+NikitaBenzin@users.noreply.github.com>
2026-02-26 14:51:08 +0100

    Folder naming

--- src/components/Adslayouts/Layout.jsx
+++ src/components/Adslayouts/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router"
 
 import { LeftSideBar } from '../sideBars/ASDlayoutLeftSideBar/LeftSideBar'
-import { RightSideBar } from '../sideBars/ASDlayoutRightSideBar/RightSideBar'
+import { RightSideBar } from '../sideBars/layoutRightSideBar/RightSideBar'
 import "./Layout.css"
 
 export default function Layout() {
