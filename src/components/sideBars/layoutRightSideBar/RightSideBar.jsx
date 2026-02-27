27a9bc1417913790390850262f0a6b649ba3a917
NikitaBenzin <83305705+NikitaBenzin@users.noreply.github.com>
2026-02-27 17:22:33 +0100

    Add vercel config file

--- src/components/sideBars/layoutRightSideBar/RightSideBar.jsx
+++ src/components/sideBars/layoutRightSideBar/RightSideBar.jsx
@@ -1,27 +1,27 @@
 "use client"
 
 import { Search, UserCog } from "lucide-react"
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 import "./RightSideBar.css"
-import { UsersSection } from './usersSection/UsersSection.jsx'
+import { UsersSection } from "./usersSection/UsersSection.jsx"
 
 export function RightSideBar() {
-  const [query, setQuery] = useState('')
-  const [debouncedQuery, setDebouncedQuery] = useState(''); // Для запроса в API
+  const [query, setQuery] = useState("")
+  const [debouncedQuery, setDebouncedQuery] = useState("")
 
   useEffect(() => {
     const handler = setTimeout(() => {
-      const trimmed = query.trim();
+      const trimmed = query.trim()
 
       if (trimmed.length > 0) {
-        setDebouncedQuery(trimmed);
+        setDebouncedQuery(trimmed)
       } else {
-        setDebouncedQuery(''); 
+        setDebouncedQuery("")
       }
-    }, 700);
+    }, 700)
 
-    return () => clearTimeout(handler);
-  }, [query]);
+    return () => clearTimeout(handler)
+  }, [query])
 
   return (
     <aside className="rightNavBar">
@@ -30,14 +30,19 @@ export function RightSideBar() {
           <div className="searchIcon" aria-hidden="true">
             <Search size={18} />
           </div>
-          <input type="text" placeholder="Search for a friend" value={query} onChange={e => setQuery(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Search for a friend"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
         </div>
         <button className="searchFilterBtn">
           <UserCog size={18} />
         </button>
       </section>
-      
-        <UsersSection query={debouncedQuery} />
+
+      <UsersSection query={debouncedQuery} />
     </aside>
   )
 }
